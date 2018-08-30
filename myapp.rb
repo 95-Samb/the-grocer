9ca6f6b414ac390d2e5caafd554889c33fa55a66
Sam <samuelbrown201195@hotmail.co.uk>
2018-08-30 15:52:34 +0100

    more than homepage atleast

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,26 @@
 # myapp.rb
 require 'bundler/setup'
 require 'sinatra'
+require 'csv'
+
 get '/' do
   erb :hello
 end
 
 get '/inventory' do
 	erb :inventory_table
+end
+
+get '/test' do
+	erb :test
+end
+
+get '/csv' do 
+    #content_type 'application/csv'
+    #attachment "test.csv"
+    @@csv_string = CSV.generate do |csv|
+        csv << ["row", "of", "CSV", "data",]
+        csv << ["another", "row"]
+        # ...
+    end    
 end
